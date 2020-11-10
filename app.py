78252996a54d2cdbd7ce60afe2ace2fb0ae4a115
Diwakar121 <diwakargour121@gmail.com>
2020-11-10 19:09:36 +0530

    3rd commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # main.py
 # import the necessary packages
 from flask import Flask, render_template, Response,request
-from camera import VideoCamera
+# from camera import VideoCamera
 app = Flask(__name__)
 @app.route('/')
 def index():
@@ -15,10 +15,10 @@ def gen(camera):
         frame = camera.get_frame()
         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-@app.route('/video_feed')
-def video_feed():
-    ft=request.args.get('filter_type')
-    return Response(gen(VideoCamera(filter_type=ft)),mimetype='multipart/x-mixed-replace; boundary=frame')
+# @app.route('/video_feed')
+# def video_feed():
+#     ft=request.args.get('filter_type')
+#     return Response(gen(VideoCamera(filter_type=ft)),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
